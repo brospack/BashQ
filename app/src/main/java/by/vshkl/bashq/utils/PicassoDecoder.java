268cbbee978030173b73e154281466c0fbc66a11
Pavel Vashkel <jgugn1@gmail.com>
2016-08-06 12:41:04 +0300

    Implemented PicassoDecoder and PicassoRegionDecoder in order to load images into SubsamplingScaleImageView with help of Picasso

---
+++ app/src/main/java/by/vshkl/bashq/utils/PicassoDecoder.java
@@ -0,0 +1,30 @@
+package by.vshkl.bashq.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import com.davemorrissey.labs.subscaleview.decoder.ImageDecoder;
+import com.squareup.picasso.MemoryPolicy;
+import com.squareup.picasso.Picasso;
+
+public class PicassoDecoder implements ImageDecoder
+{
+    private String tag;
+    private Picasso picasso;
+
+    public PicassoDecoder(String tag, Picasso picasso) {
+        this.tag = tag;
+        this.picasso = picasso;
+    }
+
+    @Override
+    public Bitmap decode(Context context, Uri uri) throws Exception {
+        return picasso
+                .load(uri)
+                .tag(tag)
+                .config(Bitmap.Config.ARGB_8888)
+                .memoryPolicy(MemoryPolicy.NO_CACHE)
+                .get();
+    }
+}
