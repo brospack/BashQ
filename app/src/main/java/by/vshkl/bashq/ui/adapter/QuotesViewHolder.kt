0d5e2b71cbcac512f6b9a04fcafc16f06aa0f8f4
Pavel Vashkel <jgugn1@gmail.com>
2016-10-29 00:46:55 +0300

    Color tint for clicked item's buttons added

--- app/src/main/java/by/vshkl/bashq/ui/adapter/QuotesViewHolder.kt
+++ app/src/main/java/by/vshkl/bashq/ui/adapter/QuotesViewHolder.kt
@@ -1,8 +1,13 @@
 package by.vshkl.bashq.ui.adapter
 
+import android.content.Context
+import android.support.v4.content.ContextCompat
+import android.support.v4.graphics.drawable.DrawableCompat
 import android.support.v7.widget.RecyclerView
 import android.text.Html
 import android.view.View
+import android.widget.ImageView
+import by.vshkl.bashq.R
 import by.vshkl.bashq.model.Quote
 import by.vshkl.bashq.model.Rating
 import by.vshkl.bashq.utils.Ratings
@@ -17,6 +22,7 @@ class QuotesViewHolder(view: View, val listener: QuoteActionListener) :
             itemView.number.text = quote.id
             itemView.date.text = quote.date
             itemView.content.text = Html.fromHtml(quote.content).trim()
+            dropTint(itemView.context, itemView.votePlus, itemView.voteMinus, itemView.voteOld, 1)
 
             if (quote.rating.equals("")) {
                 itemView.votes.visibility = View.GONE
@@ -29,6 +35,7 @@ class QuotesViewHolder(view: View, val listener: QuoteActionListener) :
                     val ratingObj = Ratings.updateRating(Rating(quote.rating, quote.voteCount), 1)
                     quote.voteCount = ratingObj.voteCount
                     itemView.rating.text = ratingObj.rating
+                    tintDrawables(itemView.context, itemView.votePlus, itemView.voteMinus, itemView.voteOld, 1)
                 }
 
                 itemView.voteMinus.setOnClickListener {
@@ -36,6 +43,7 @@ class QuotesViewHolder(view: View, val listener: QuoteActionListener) :
                     val ratingObj = Ratings.updateRating(Rating(quote.rating, quote.voteCount), -1)
                     quote.voteCount = ratingObj.voteCount
                     itemView.rating.text = ratingObj.rating
+                    tintDrawables(itemView.context, itemView.votePlus, itemView.voteMinus, itemView.voteOld, 2)
                 }
 
                 itemView.voteOld.setOnClickListener {
@@ -43,9 +51,36 @@ class QuotesViewHolder(view: View, val listener: QuoteActionListener) :
                     val ratingObj = Ratings.updateRating(Rating(quote.rating, quote.voteCount), 0)
                     quote.voteCount = ratingObj.voteCount
                     itemView.rating.text = ratingObj.rating
+                    tintDrawables(itemView.context, itemView.votePlus, itemView.voteMinus, itemView.voteOld, 3)
                 }
 
                 itemView.setOnLongClickListener { listener.share(content, link) }
+            }
+        }
+    }
+
+    fun dropTint(context: Context, plus: ImageView, minus: ImageView, old: ImageView, which: Int) {
+        DrawableCompat.setTint(plus.drawable, ContextCompat.getColor(context, R.color.colorSecondaryText))
+        DrawableCompat.setTint(minus.drawable, ContextCompat.getColor(context, R.color.colorSecondaryText))
+        DrawableCompat.setTint(old.drawable, ContextCompat.getColor(context, R.color.colorSecondaryText))
+    }
+
+    fun tintDrawables(context: Context, plus: ImageView, minus: ImageView, old: ImageView, which: Int) {
+        when (which) {
+            1 -> {
+                DrawableCompat.setTint(plus.drawable, ContextCompat.getColor(context, R.color.colorAccent))
+                DrawableCompat.setTint(minus.drawable, ContextCompat.getColor(context, R.color.colorSecondaryText))
+                DrawableCompat.setTint(old.drawable, ContextCompat.getColor(context, R.color.colorSecondaryText))
+            }
+            2 -> {
+                DrawableCompat.setTint(plus.drawable, ContextCompat.getColor(context, R.color.colorSecondaryText))
+                DrawableCompat.setTint(minus.drawable, ContextCompat.getColor(context, R.color.colorAccent))
+                DrawableCompat.setTint(old.drawable, ContextCompat.getColor(context, R.color.colorSecondaryText))
+            }
+            3 -> {
+                DrawableCompat.setTint(plus.drawable, ContextCompat.getColor(context, R.color.colorSecondaryText))
+                DrawableCompat.setTint(minus.drawable, ContextCompat.getColor(context, R.color.colorSecondaryText))
+                DrawableCompat.setTint(old.drawable, ContextCompat.getColor(context, R.color.colorAccent))
             }
         }
     }
