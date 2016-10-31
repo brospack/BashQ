f9d70cc637940d9929350faebf297cd07c97c407
Pavel Vashkel <jgugn1@gmail.com>
2016-10-31 12:31:14 +0300

    Repository interface created

---
+++ mvp/src/main/java/by/vshkl/repository/Repository.java
@@ -0,0 +1,21 @@
+package by.vshkl.repository;
+
+import java.util.List;
+
+import by.vshkl.mvp.model.Comic;
+import by.vshkl.mvp.model.Quote;
+import by.vshkl.mvp.model.ResponseWrapper;
+import io.reactivex.Observable;
+
+public interface Repository {
+
+    Observable<ResponseWrapper<List<Quote>>> getQuotes();
+
+    Observable<Quote> getQuote();
+
+    Observable<Boolean> voteQuote(Quote.VoteState requiredVoteStatus);
+
+    Observable<ResponseWrapper<List<Comic>>> getComics();
+
+    Observable<Comic> getComic();
+}
