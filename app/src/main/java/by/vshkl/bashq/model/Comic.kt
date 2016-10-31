c125151cd36a9d5c08402d64a53323b7fba393d7
Pavel Vashkel <jgugn1@gmail.com>
2016-10-31 12:01:10 +0300

    Clean up

--- app/src/main/java/by/vshkl/bashq/model/Comic.kt
+++
@@ -1,6 +0,0 @@
-package by.vshkl.bashq.model
-
-data class Comic(
-        val thumbLink: String,
-        val comicLink: String
-)
--- app/src/main/java/by/vshkl/bashq/view/GalleryActionListener.kt
+++
@@ -1,8 +0,0 @@
-package by.vshkl.bashq.view
-
-import android.view.View
-
-interface GalleryActionListener {
-
-    fun onGalleryItemClicked(comicUrl: String, view: View)
-}
