e66b0a23736d3c3c19cb3a64de4f107674e2258c
Pavel Vashkel <jgugn1@gmail.com>
2016-11-04 11:27:16 +0300

    Added activity to handle copy to clipboard action in share chooser

---
+++ app/src/main/java/by/vshkl/bashq/ui/acticity/CopyToClipboardActivity.java
@@ -0,0 +1,31 @@
+package by.vshkl.bashq.ui.acticity;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import by.vshkl.bashq.R;
+
+public class CopyToClipboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        finish();
+
+        ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(Intent.EXTRA_TEXT) && intent.getType().equals("text/plain")) {
+            String contentToCopy = intent.getStringExtra(Intent.EXTRA_TEXT);
+            ClipData clipData = ClipData.newPlainText("", contentToCopy);
+            clipboardManager.setPrimaryClip(clipData);
+
+            Toast.makeText(this, R.string.message_copied_to_clipboard, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
