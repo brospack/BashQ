bd52204296ce1009dfb6a481b50367878cce1f66
Pavel Vashkel <jgugn1@gmail.com>
2016-11-09 17:26:11 +0300

    DI components updated

--- app/src/main/java/by/vshkl/bashq/injection/component/ComicsComponent.java
+++ app/src/main/java/by/vshkl/bashq/injection/component/ComicsComponent.java
@@ -1,20 +1,14 @@
 package by.vshkl.bashq.injection.component;
 
-import android.content.Context;
-
-import by.vshkl.bashq.injection.module.ActivityModule;
 import by.vshkl.bashq.injection.module.ComicsModule;
-import by.vshkl.bashq.injection.module.NavigationModule;
 import by.vshkl.bashq.injection.scope.PerActivity;
-import by.vshkl.bashq.ui.activity.ComicsActivity;
+import by.vshkl.bashq.ui.fragment.ComicsFragment;
 import dagger.Component;
 
 @PerActivity
 @Component(dependencies = ApplicationComponent.class,
-        modules = {ActivityModule.class, ComicsModule.class, NavigationModule.class})
+        modules = {ComicsModule.class})
 public interface ComicsComponent {
 
-    void inject(ComicsActivity ComicsActivity);
-
-    Context context();
+    void inject(ComicsFragment comicsFragment);
 }
