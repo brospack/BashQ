2648cd735abde89a12c5af6a190d1bf28a5f5140
Pavel Vashkel <jgugn1@gmail.com>
2016-11-10 18:25:46 +0300

    Quote entity and mappers added

---
+++ app/src/main/java/by/vshkl/bashq/database/model/QuoteEntity.java
@@ -0,0 +1,56 @@
+package by.vshkl.bashq.database.model;
+
+import com.raizlabs.android.dbflow.annotation.Column;
+import com.raizlabs.android.dbflow.annotation.PrimaryKey;
+import com.raizlabs.android.dbflow.annotation.Table;
+import com.raizlabs.android.dbflow.structure.BaseModel;
+
+import by.vshkl.bashq.database.AppDatabase;
+
+@Table(database = AppDatabase.class)
+public class QuoteEntity extends BaseModel {
+
+    @PrimaryKey
+    String id;
+
+    @Column
+    String date;
+
+    @Column
+    String link;
+
+    @Column
+    String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
