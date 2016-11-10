0806f31e42ae4f64b9e0931af3fa7644fa7c4c1e
Pavel Vashkel <jgugn1@gmail.com>
2016-11-10 18:25:22 +0300

    Database added

---
+++ app/src/main/java/by/vshkl/bashq/database/AppDatabase.java
@@ -0,0 +1,11 @@
+package by.vshkl.bashq.database;
+
+import com.raizlabs.android.dbflow.annotation.Database;
+
+@Database(name = AppDatabase.NAME, version = AppDatabase.VERSION)
+public class AppDatabase {
+
+    public static final String NAME = "BashQDatabase";
+
+    public static final int VERSION = 1;
+}
